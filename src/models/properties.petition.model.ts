8cdd8fa3c1446c922b9b3dc2e94f015b1b1cc28e
Perret Pierre-Yves <pierreyves.perret@gmail.com>
2021-06-19 23:07:46 +0200

    svg

--- src/models/properties.petition.model.ts
+++ src/models/properties.petition.model.ts
@@ -3,6 +3,7 @@ export class PropertiesPetition {
   constructor(public owner: string,
               public description: string,
               public tag: string,
-              public title: string) {
+              public title: string,
+              public nbSignature: number) {
   }
 }
